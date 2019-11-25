94e9a565d31e44ef5bd329db0f16dc0142af5c0d
Юрий Михейкин <umiheikin@yandex.ru>
2019-11-25 13:47:02 +0300

    add todobackend

---
+++ todobackend/src/todobackend/settings/test.py
@@ -0,0 +1,32 @@
+from base import *
+import os
+
+#Installed apps
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+	'--verbosity=2',
+	'--nologcapture',
+	'--with-coverage',
+	'--cover-package=todo',
+	'--with-spec',
+	'--spec-color',
+	'--with-xunit',
+	'--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+	'--cover-xml',
+	'--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
+#Database
+#https://docs.djangoproject.com/en/2.2/ref/databases
+DATABASES = {
+	'default': {
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
+	'USER': os.environ.get('MYSQL_USER','todo'),
+	'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+	'HOST': os.environ.get('MYSQL_HOST','localhost'),
+	'PORT': os.environ.get('MYSQL_PORT','3306'),
+	}
+}
